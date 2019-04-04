49a4a74fefa2a503e0370c65092682ef583f213f
AndreyVMishchenko <49276201+AndreyVMishchenko@users.noreply.github.com>
2019-04-04 16:27:08 +0300

    Update Logger.java

--- Zone3000/src/main/java/conf/Logger.java
+++ Zone3000/src/main/java/conf/Logger.java
@@ -6,61 +6,30 @@ public class Logger extends Variables {
 	Config configuration = new Config();
 		
 	public void name(String string){	
-		//Вывод названия теста
+		//Г‚Г»ГўГ®Г¤ Г­Г Г§ГўГ Г­ГЁГї ГІГҐГ±ГІГ 
 		configuration.dateTime();
 		System.out.print("[Info]");
-		System.out.println("Тест № " + string);
+		System.out.println("Г’ГҐГ±ГІ В№ " + string);
 	}
 	
 	public void doText(String string){	
-		//Вывод текста
+		//Г‚Г»ГўГ®Г¤ ГІГҐГЄГ±ГІГ 
 		configuration.dateTime();
 		System.out.print("[Info]");
 		System.out.println(string);
 	}	
 	
 	public void doClick(String string){		
-		//Вывод нажатия на элемент
+		//Г‚Г»ГўГ®Г¤ Г­Г Г¦Г ГІГЁГї Г­Г  ГЅГ«ГҐГ¬ГҐГ­ГІ
 		configuration.dateTime();
 		System.out.print("[Click]");
-		System.out.println("Нажимаем на кнопку " + string);
+		System.out.println("ГЌГ Г¦ГЁГ¬Г ГҐГ¬ Г­Г  ГЄГ­Г®ГЇГЄГі " + string);
 	}	
 	
 	public void doInput(String string){		
-		//Вывод ввода значения
+		//Г‚Г»ГўГ®Г¤ ГўГўГ®Г¤Г  Г§Г­Г Г·ГҐГ­ГЁГї
 		configuration.dateTime();
 		System.out.print("[Input]");
-		System.out.println("Набираем/Устанавливаем значение в поле " + string);
+		System.out.println("ГЌГ ГЎГЁГ°Г ГҐГ¬/Г“Г±ГІГ Г­Г ГўГ«ГЁГўГ ГҐГ¬ Г§Г­Г Г·ГҐГ­ГЁГҐ Гў ГЇГ®Г«ГҐ " + string);
 	}	
-	
-	
-	
-	public void doCheck(){	
-		//Вывод проверки
-		configuration.dateTime();
-		System.out.print("[Check]");
-		System.out.println("Проверяем результат: ");		
-	}
-	
-	
-	
-	public void ignoreAdmin(){		
-		if (account.equals("adm")){
-			System.out.println("Для администратора данный тест игнорируется");				
-		}			
-	}
-	
-	public void ignoreManager(){		
-		if (account.equals("man")){
-			System.out.println("Для менеджера данный тест игнорируется");
-			driver.quit();
-		}
-	}
-	
-	public void ignorePolzovatel(){		
-		if (account.equals("pol")){
-			System.out.println("Для пользователя данный тест игнорируется");
-			driver.quit();
-		}
-	}
 }
